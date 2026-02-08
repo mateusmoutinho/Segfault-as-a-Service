a4069c42288cbd2e8fadacc346445d3dd7600cb1
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-07 22:52:48 -0300

    att

--- app.c
+++ app.c
@@ -1,11 +1,19 @@
 
-typedef struct appdeps{
-    const char *route;
-    const char *(*get_headder)( const char *key);
-    const char *(*get_query_param)( const char *key);
-    const unsigned char *(*read_body)(long size);
+typedef void request;
+typedef void response;
 
-    
+typedef struct appdeps{
+    const request *request;
+    const char *route;
+    const char *(*get_headder)(const request *request, const char *key);
+    const char *(*get_query_param)(const request *request, const char *key);
+    const unsigned char *(*read_body)(const request *request, long size);
+    const response *(*send_any)(const unsigned char *content,long content_size,int status_code);
+    const response *(*send_file)(const char *path,int status_code);
+
+
+
+
 
 
 } appdeps;
