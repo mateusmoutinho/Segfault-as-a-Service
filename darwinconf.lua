477aece850851546fe9bd7e97db311e1b27bce6b
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-08 18:50:44 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,13 +1,13 @@
-PROJECT_NAME = "Single-File-Web-Server"
+PROJECT_NAME = "Capsule"
 CONTANIZER   = darwin.argv.get_flag_arg_by_index({ "provider", "contanizer" }, 1, "podman")
 PORT  = darwin.argv.get_flag_arg_by_index({ "port" }, 1, "5000")
 VERSION      = "0.1.0"
 LICENSE      = "Unlicense"
-URL          = "https://github.com/mateusmoutinho/Single-File-Web-Server"
-DESCRIPITION = "Single-File-Web-Server - C-Based Web Server Template for vibecoding"
+URL          = "https://github.com/mateusmoutinho/Capsule"
+DESCRIPITION = "Capsule - C-Based Web Server Template for vibecoding"
 FULLNAME     = "Mateus Moutinho"
 EMAIL        = "[EMAIL_ADDRESS]"
-SUMARY       = "Single-File-Web-Server - C-Based Web Server Template for vibecoding"
+SUMARY       = "Capsule - C-Based Web Server Template for vibecoding"
 LANGUAGE     = "c"
 COMPILER = darwin.argv.get_flag_arg_by_index({ "compiler" }, 1)
 if not COMPILER and LANGUAGE == "cpp" then
